9fd2ada23957251ea2ca21c520f40f0c28397c18
Seba <sevaledesma@gmail.com>
2023-04-13 08:35:05 -0300

    Habilitando cors en app

--- src/main/java/com/portfolio/back/PortfolioBackendApplication.java
+++ src/main/java/com/portfolio/back/PortfolioBackendApplication.java
@@ -23,7 +23,7 @@ public class PortfolioBackendApplication implements CommandLineRunner {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://portfolio-frontend-bfcaf.firebaseapp.com");
+				registry.addMapping("/**").allowedOrigins("https://portfolio-frontend-bfcaf.firebaseapp.com");
 			}
 		};
 	}
